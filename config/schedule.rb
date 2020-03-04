df646c263eb75ed23ee367b2f4e345e01d73ccba
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-03-04 16:11:08 +0000

    Add an example whenever schedule.rb
    This is a very basic example of a whenever schedule.rb. This differs slightly from our normal schedule.rb files because
    
    - we don't tell whenever to log to a specific file
    - we tell whenever to not do anything clever when running the commands i.e. just run the command as added
    
    This is all because we are running the job within a container and using [supercronic](https://github.com/aptible/supercronic) as our job runner rather than cron (the next commit will go into the details of that!)

---
+++ config/schedule.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Learn more: http://github.com/javan/whenever
+
+set :job_template, nil
+
+# Only one of the AWS app servers has a role of "db"
+# see https://gitlab-dev.aws-int.defra.cloud/open/rails-deployment/blob/master/config/deploy.rb#L69
+# so only creating cronjobs on that server, otherwise all jobs would be duplicated everyday!
+
+# Send a test email to confirm cron jobs are working
+every 1.minute do
+  command "bundle exec rake email:test"
+end
